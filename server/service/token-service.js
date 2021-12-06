2b91dc23d7c2319473cc95a255887e1400952d79
Oleksandr <kruushik89@gmail.com>
2021-12-06 22:04:12 +0200

    token key

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -1,5 +1,10 @@
-class TokenService {
+const jwt = require("jsonwebtoken");
 
+class TokenService {
+    generateTokens(payload) {
+        //expiresIn - час життя токена
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, expiresIn: "30m")
+    }
 }
 
 module.exports = new TokenService();
